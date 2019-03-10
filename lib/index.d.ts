6715530a36c14778ae46e82906bbf0a83cce83dd
Pablo <pablo.godino@arup.com>
2019-03-10 16:20:09 +0000

    Update class name

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-export declare abstract class DebugLogger {
+export declare abstract class Logger {
     static log(message?: any, ...optionalParams: any[]): void;
     private static LOGGER_KEY;
     private static LOG_ENABLED;
